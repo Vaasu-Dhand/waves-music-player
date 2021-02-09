2a31e7a7d31c3d6d8580371c21a11ecdf6f0530e
Vaasu Dhand <dhandvaasu@gmail.com>
2021-02-09 16:58:47 +0530

    Waver Music Player init

---
+++ src/components/Player.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Player() {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start TIme</p>
+        <input type="range" name="" id=""/>
+        <p>End Time</p>
+      </div>
+      <div className="play-control"></div>
+    </div>
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Player from './Player';
+import Song from './Song'
+
+export { Player, Song };
