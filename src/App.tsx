b3fbb46ba9741416e7709273fc3fa236bce86edf
Vaasu Dhand <dhandvaasu@gmail.com>
2021-02-11 23:12:43 +0530

    Finished App

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,22 @@ import './styles/app.scss';
 import { SongProvider, RefProvider, NavProvider } from './context';
 
 function App() {
+  
   return (
     <div className="App">
       <NavProvider>
         <Nav />
-        <SongProvider>
-          <Song />
-          <RefProvider>
-            <Player />
-            <Library />
-          </RefProvider>
-        </SongProvider>
+      <SongProvider>
+        <Song />
+        <RefProvider>
+          <Player />
+          <Library />
+        </RefProvider>
+      </SongProvider>
       </NavProvider>
     </div>
   );
 }
 
 export default App;
+
