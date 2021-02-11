715eafb2a5ec2f2de2a21c2dcfa52a72f6ecbffb
Vaasu Dhand <dhandvaasu@gmail.com>
2021-02-11 18:35:28 +0530

    local Song App Finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,21 @@ import { Song, Player, Library, Nav } from './components/index';
 import './styles/app.scss';
 import { SongProvider, RefProvider, NavProvider } from './context';
 
-
 function App() {
-  
   return (
     <div className="App">
       <NavProvider>
         <Nav />
-      <SongProvider>
-        <Song />
-        <RefProvider>
-          <Player />
-          <Library />
-        </RefProvider>
-      </SongProvider>
+        <SongProvider>
+          <Song />
+          <RefProvider>
+            <Player />
+            <Library />
+          </RefProvider>
+        </SongProvider>
       </NavProvider>
     </div>
   );
 }
 
 export default App;
-
